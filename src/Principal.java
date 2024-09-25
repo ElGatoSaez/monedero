b71e8e7bd358594b1d9247888cc4c7eb7f56bb72
Sebastián Sáez <10396037+ElGatoSaez@users.noreply.github.com>
2024-09-25 20:29:50 -0300

    switching

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,7 @@
 public class Principal {
     public static void main(String[] args) {
+        String separator = "************************";
+        System.out.println(separator);
+        System.out.println();
     }
 }
