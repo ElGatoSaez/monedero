5c422b2308c1369c7fddedb15254c883e3a2a438
Sebastián Sáez <10396037+ElGatoSaez@users.noreply.github.com>
2024-09-25 23:17:04 -0300

    Desafío completado de forma rápida, se viene versión refactorizada.

--- src/Caller.java
+++ src/Caller.java
@@ -1,5 +1,39 @@
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Properties;
+
 public class Caller {
+    private static final String apiSite = "https://v6.exchangerate-api.com/v6/";
+    private static String apiKey;
 
+    public static void configLoader() {
+        Properties properties = new Properties();
+        String configPath = "config.properties";
+        try (FileInputStream input = new FileInputStream(configPath)) {
+            properties.load(input);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        apiKey = properties.getProperty("api_key");
+    }
 
+    public static void convert(double cantidad, String from, String to) throws IOException, InterruptedException {
+        configLoader();
+        String callSite = apiSite + apiKey + "/pair/" + from + "/" + to + "/" + cantidad;
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(callSite)).build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        var json = response.body();
+        JsonObject jsonObject = JsonParser.parseString(json).getAsJsonObject();
+        double conversion = jsonObject.get("conversion_result").getAsDouble();
+        System.out.println("El valor de " + cantidad + " " + from + " corresponde al valor final de " + to + " " + conversion);
+    }
 
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,7 +1,55 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Principal {
-    public static void main(String[] args) {
-        String separator = "************************";
-        System.out.println(separator);
-        System.out.println();
+    public static void main(String[] args) throws IOException, InterruptedException {
+        while (true) {
+            Scanner teclado = new Scanner(System.in);
+            String separator = "************************";
+            String msg1 = "Ingrese la cantidad a convertir: ";
+            System.out.println(separator);
+            System.out.println("Conversor de moneda\n");
+            System.out.println("1) Dólar -> Peso argentino");
+            System.out.println("2) Peso argentino -> Dólar");
+            System.out.println("3) Dólar -> Real brasileño");
+            System.out.println("4) Real brasileño -> Dólar");
+            System.out.println("5) Dólar -> Peso colombiano");
+            System.out.println("6) Peso colombiano -> Dólar");
+            System.out.println("7) Salir\n");
+            System.out.println("Elija una opción válida");
+            System.out.println(separator);
+            int opcionValida = teclado.nextInt();
+            if (opcionValida == 7) {
+                System.out.println("Hasta luego!");
+                break;
+            }
+            switch (opcionValida) {
+                case 1:
+                    System.out.println(msg1);
+                    Caller.convert(teclado.nextDouble(), "USD", "ARS");
+                    break;
+                case 2:
+                    System.out.println(msg1);
+                    Caller.convert(teclado.nextDouble(), "ARS", "USD");
+                    break;
+                case 3:
+                    System.out.println(msg1);
+                    Caller.convert(teclado.nextDouble(), "USD", "BRL");
+                    break;
+                case 4:
+                    System.out.println(msg1);
+                    Caller.convert(teclado.nextDouble(), "BRL", "USD");
+                    break;
+                case 5:
+                    System.out.println(msg1);
+                    Caller.convert(teclado.nextDouble(), "USD", "COP");
+                    break;
+                case 6:
+                    System.out.println(msg1);
+                    Caller.convert(teclado.nextDouble(), "COP", "USD");
+                    break;
+
+            }
+        }
     }
 }
